701143ed8f19c4db8de0dd66e4192ce1156d2fb4
Hidden Refuge <hidden-refuge@users.noreply.github.com>
2016-07-05 17:23:35 +0200

    Create demp.sh

---
+++ demp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+############################################################################
+# Debian + Nginx + MySQL + PHP                                             #
+# Version: 0.1 Build 1                                                     #
+# Branch: Dev                                                              #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Author: Hidden Refuge (© 2016)                                           #
+# License: MIT License                                                     #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# SPI Wiki: https://github.com/hidden-refuge/demp                          #
+############################################################################
+
+# Declaring a few misc variables
+dempversion=0.1 # DEMP version
+dempbuild=1 # DEMP build number
+dempbranch=Dev # DEMP build branch
+
+# Function to remove previous Apache, Nginx, MySQL and PHP installations
+cleanup ()  {
+# coming soon
+}
+
+# Function to install the latest stable Nginx version
+instnginx ()  {
+# coming soon
+}
