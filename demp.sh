0992dda48e59a212c262f4faa63362963c4fc4a3
Hidden Refuge <hidden-refuge@users.noreply.github.com>
2016-07-11 20:20:07 +0200

    Update demp.sh

--- demp.sh
+++ demp.sh
@@ -2,26 +2,22 @@
 
 ############################################################################
 # Debian + Nginx + MySQL + PHP                                             #
-# Version: 0.1 Build 1                                                     #
+# Version: 0.2                                                             #
 # Branch: Dev                                                              #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 # Author: Hidden Refuge (© 2016)                                           #
 # License: MIT License                                                     #
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# SPI Wiki: https://github.com/hidden-refuge/demp                          #
 ############################################################################
 
-# Declaring a few misc variables
-dempversion=0.1 # DEMP version
-dempbuild=1 # DEMP build number
-dempbranch=Dev # DEMP build branch
-
-# Function to remove previous Apache, Nginx, MySQL and PHP installations
-cleanup ()  {
-# coming soon
-}
 
 # Function to install the latest stable Nginx version
 instnginx ()  {
-# coming soon
+  wget -O /tmp/nginx_signing.key http://nginx.org/keys/nginx_signing.key # downloading nginx GPG key
+  apt-key add /tmp/nginx_signing.key # installing nginx GPG key
+  rm -rf /tmp/nginx_signing.key # removing downloaded GPG key file
+  echo "deb http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/sources.list # adding official nginx stable version repository
+  # uncomment the line below if you need the source code for the latest stable nginx version
+  #echo "deb-src http://nginx.org/packages/debian/ jessie nginx" >> /etc/apt/sources.list # adding source for official nginx stable version
+  apt-get update # updating apt-get package database and sources
+  apt-get install nginx -y # installing nginx
 }
